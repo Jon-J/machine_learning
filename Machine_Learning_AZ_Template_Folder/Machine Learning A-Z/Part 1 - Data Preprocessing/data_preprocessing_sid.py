5d6ce67c4a8913f6a1f42a87f733214af7c9fe14
Jon-J <bv.sheetalkumar@gmail.com>
2018-10-02 21:52:40 +0530

    Adding code for Feature Scaling.

--- Machine_Learning_AZ_Template_Folder/Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_sid.py
+++ Machine_Learning_AZ_Template_Folder/Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_sid.py
@@ -27,8 +27,14 @@ onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
 
 labelencode_Y = LabelEncoder()
-Y = labelencode_Y.fit_transform(Y)
+Y = labelencode_Y.fit_transform(Y)   
 
 # Splitting the dataset into Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
