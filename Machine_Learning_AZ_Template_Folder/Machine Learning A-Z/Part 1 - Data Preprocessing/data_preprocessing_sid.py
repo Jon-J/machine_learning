74e626e8826de85a820bf0fdea5920912e42f1ae
Jon-J <bv.sheetalkumar@gmail.com>
2018-10-02 19:33:49 +0530

    Learnt about splitting dataset

--- Machine_Learning_AZ_Template_Folder/Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_sid.py
+++ Machine_Learning_AZ_Template_Folder/Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_sid.py
@@ -19,11 +19,16 @@ imputer = Imputer(missing_values="NaN", strategy='mean', axis=0)
 imputer = imputer.fit(X[:,1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencode_X = LabelEncoder()
 X[:, 0] = labelencode_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
 
 labelencode_Y = LabelEncoder()
 Y = labelencode_Y.fit_transform(Y)
+
+# Splitting the dataset into Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
